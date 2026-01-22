f1e8abb8f50edb4d25e7965011bec3fbcaba3c55
karl2603 <arvindrajkarl@gmail.com>
2026-01-22 16:05:50 +0530

    Buffered Reader

---
+++ Writer/BufferedReader/bufferedReader.java
@@ -0,0 +1,21 @@
+package Writer.BufferedReader;
+
+import java.io.FileReader;
+import java.io.BufferedReader;
+
+public class bufferedReader {
+    public static void main(String args[]){
+        try{
+        FileReader fr = new FileReader("bufferedReader.txt");
+        BufferedReader br = new BufferedReader(fr);
+        String Line = br.readLine();
+        while(Line != null){
+            System.out.println(Line);
+            Line = br.readLine();
+        }
+    } 
+    catch(Exception e){
+System.out.println("Error");
+    }
+    }
+}
